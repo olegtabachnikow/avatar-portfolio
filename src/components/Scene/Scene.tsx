6be1bfb7a8521c16b0ba38da23bc56b5e51d6a5f
OlegTabachnikow <eskelll4ik@gmail.com>
2024-06-08 20:43:00 +0300

    updated camera on intro section

--- src/components/Scene/Scene.tsx
+++ src/components/Scene/Scene.tsx
@@ -1,5 +1,4 @@
 import { FC, useRef, useState } from 'react';
-import * as THREE from 'three';
 import { OrbitControls, Html } from '@react-three/drei';
 import Avatar from '../Avatar/Avatar';
 import Floor from '../Floor/Floor';
@@ -11,17 +10,9 @@ const Scene: FC = () => {
   const controlsRef = useRef<OrbitControlsType>(null);
 
   useFrame(() => {
-    // state.camera.position.lerp(
-    //   new THREE.Vector3(0, isMoved ? 2.5 : 1.2, isMoved ? 2 : 3),
-    //   0.05
-    // );
     if (controlsRef.current) {
       controlsRef.current.object.position.lerp(
-        { x: 0, y: isMoved ? 1.7 : 3, z: isMoved ? 1.5 : 0.5 },
-        0.05
-      );
-      controlsRef.current.target.lerp(
-        new THREE.Vector3(0, isMoved ? 1.5 : 2.8, isMoved ? 1.3 : 0.4),
+        { x: 0, y: isMoved ? 2 : 2, z: isMoved ? 2.5 : 1.5 },
         0.05
       );
     }
@@ -29,20 +20,18 @@ const Scene: FC = () => {
   return (
     <>
       <OrbitControls
+        target={[0, 1.5, 0]}
         ref={controlsRef}
-        target={[0, 1, 0]}
         enablePan={false}
         enableRotate={false}
         enableZoom={false}
       />
-      <axesHelper args={[5]} />
       <Html position={[0, 1, 0]}>
         <div style={{ color: '#fff', transform: 'translate(300px, -200px)' }}>
           {isMoved ? 'Active' : 'Inactive'}
           <button onClick={() => setIsMoved((state) => !state)}>BTN</button>
         </div>
       </Html>
-      {/* <PerspectiveCamera makeDefault position={[0, 1.2, 2]} /> */}
       <ambientLight intensity={0.5} />
       <directionalLight intensity={4.5} position={[0, 2, 3]} />
       <group position={[0, 0, 0]}>
